b5091693482df09bb6081b0603959a7f4a87a568
Killian Supinski <ksupinsk@f4r11s5.clusters.42paris.fr>
2025-12-30 17:26:15 +0100

    delete .d / .o

--- objs/main.d
+++
@@ -1,8 +0,0 @@
-objs/main.o: srcs/main.c srcs/../includes/cube.h \
-  includes/../libft/libft.h libft/./get_next_line.h
-
-srcs/../includes/cube.h:
-
-includes/../libft/libft.h:
-
-libft/./get_next_line.h:
--- objs/parsing.d
+++
@@ -1,8 +0,0 @@
-objs/parsing.o: srcs/parsing.c srcs/../includes/cube.h \
-  includes/../libft/libft.h libft/./get_next_line.h
-
-srcs/../includes/cube.h:
-
-includes/../libft/libft.h:
-
-libft/./get_next_line.h:
